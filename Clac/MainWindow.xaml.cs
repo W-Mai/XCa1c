969e301a64de5e7718c7c4d823b17be0b2d71d50
W-Mai <1341398182@qq.com>
2019-10-28 22:59:02 +0800

    完善数字输入和退格以及清平功能

--- Clac/MainWindow.xaml.cs
+++ Clac/MainWindow.xaml.cs
@@ -20,26 +20,69 @@ namespace Clac
     /// </summary>
     /// 
 
-    public partial class MainWindow : Window
-    {
-        private string NowOperator = "";
-        private int Result = 0;
+    public partial class MainWindow : Window {
+        private const string NOOPERATOR = "NULL";
+        private string ContenOfInputVal = "0";
+        private string NowOperatorVal = NOOPERATOR;
+        private double Result = 0.0f;
+        private List<string> OPList= new List<string>{ "btPlus", "btMinus", "btMultiply", "btDivide" };
+
+        public string ContentOfInput {
+            get => ContenOfInputVal; set {
+                ContenOfInputVal = value;
+                ContentBox.Text = value;
+            }
+        }
+
+        public string NowOperator { get => NowOperatorVal; set { NowOperatorVal = value; ShowOperator(value); } }
+
         public MainWindow() {
             InitializeComponent();
+            reset();
+        }
+
+        private void reset() {
+            ContentOfInput = "0";
+            NowOperator = (string)btPlus.Content;
+            Result = 0.0f;
         }
 
         private void BtNumberClick(object sender, RoutedEventArgs e) {
             Button button = (Button)sender;
-            ContentBox.Text += button.Content;
+            if(NowOperator != NOOPERATOR) {
+                if(button.Name != "btDot" && ContentOfInput == "0")
+                    ContentOfInput = "";
+                ContentOfInput += button.Content;
+            }
+                
+
         }
 
         private void BtOpClick(object sender, RoutedEventArgs e) {
-
+            Button button = (Button)sender;
+            
+            if(OPList.Exists(exist=>exist == button.Name)){
+                NowOperator = (string)button.Content;
+            }
         }
 
+        // Finished.
         private void BtClearClick(object sender, RoutedEventArgs e) {
-
+            Button button = (Button)sender;
+            switch (button.Name) {
+                case "btClear":
+                    reset();
+                    break;
+                case "btBack":
+                    if (ContentOfInput.Length == 1) { ContentOfInput = "0"; break; };
+                    ContentOfInput = ContentOfInput.Substring(0, ContentOfInput.Length - 1);
+                    break;
+            }
         }
 
+        //Finished.
+        private void ShowOperator(string op) {
+            OpShow.Content = op;
+        }
     }
 }
