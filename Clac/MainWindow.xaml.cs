b190f7352b625d9f13565dc8f552d0ab95cc5df5
W-Mai <1341398182@qq.com>
2019-10-28 19:36:59 +0800

    添加项目文件。

---
+++ Clac/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clac
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        private string NowOperator = "";
+        private int Result = 0;
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void BtNumberClick(object sender, RoutedEventArgs e) {
+            Button button = (Button)sender;
+            ContentBox.Text += button.Content;
+        }
+
+        private void BtOpClick(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void BtClearClick(object sender, RoutedEventArgs e) {
+
+        }
+
+    }
+}
